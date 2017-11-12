35df749d67fea02708a110e787357966f4b7014b
gangpeng shu <sgp@192.168.9.104>
2017-11-12 21:53:55 +0800

    Commit

---
+++ GPNavigationController/Classes/GPRootUIView/GPTabViewController.h
@@ -0,0 +1,12 @@
+//
+//  GPTabViewController.h
+//  FBSnapshotTestCase
+//
+//  Created by sgp on 2017/11/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPTabViewController : UITabBarController
+
+@end
